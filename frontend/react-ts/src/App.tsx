ad9f2d0dd4fe7e899884b91a5e2b85ff7366029f
Dawid <dawid.zygmunt86@gmail.com>
2025-01-12 14:13:15 +0100

    react-query setup

--- frontend/react-ts/src/App.tsx
+++ frontend/react-ts/src/App.tsx
@@ -1,25 +1,31 @@
-import nubisoftLogo from './assets/nubisoft.svg'
+import { Calendar, Cloud, LocateFixedIcon } from 'lucide-react'
+import { Card } from './components/ui/card'
+import { Separator } from './components/ui/separator'
 
 function App() {
   return (
-    <div className="min-h-screen w-full flex justify-center flex-col gap-4 items-center">
+    <div className="min-h-screen w-full flex p-10 bg-[#0b0b0e]">
       <div>
-        <a
-          href="https://nubisoft.io/"
-          target="_blank"
-        >
-          <img
-            src={nubisoftLogo}
-            className=""
-            alt="Nubisoft logo"
-          />
-        </a>
+        <Card className="p-5 bg-[#242222] text-white border-0 ">
+          <span>Now</span>
+          <div className="flex items-center gap-10">
+            <span className="text-4xl">5oC </span>
+            <Cloud size={60} />
+          </div>
+          <p className="text-xs">Broken Clouds</p>
+          <Separator className="my-3" />
+          <div className="flex flex-col gap-1 text-sm">
+            <div className="flex items-center gap-2">
+              <Calendar size={15} />
+              Wed 12
+            </div>
+            <div className="flex items-center gap-2">
+              <LocateFixedIcon size={15} />
+              Gliwice, PL
+            </div>
+          </div>
+        </Card>
       </div>
-      <h1>NubiWeather</h1>
-      <section>
-        <h2>Here's your starting point.</h2>
-        <h3>Good luck! :)</h3>
-      </section>
     </div>
   )
 }
